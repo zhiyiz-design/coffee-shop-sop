c4e9419791a702e72382890db5937d9f331f0166
zoeymac <zoeymac@zoeymacdeMacBook-Air.local>
2026-04-29 06:23:51 -0700

    Configure Firebase project

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,13 @@
 window.COFFEE_SOP_FIREBASE = {
   // 把 Firebase 控制台里 Web App 的配置对象粘到这里。
-  // 例如：
-  // config: {
-  //   apiKey: "xxx",
-  //   authDomain: "xxx.firebaseapp.com",
-  //   projectId: "xxx",
-  //   storageBucket: "xxx.appspot.com",
-  //   messagingSenderId: "1234567890",
-  //   appId: "1:1234567890:web:abcdef",
-  // },
-  config: null,
+  config: {
+    apiKey: "AIzaSyBYmfJsQck8UetjkGbjAYzbeLegFVMZ1Gw",
+    authDomain: "coffee-app-430df.firebaseapp.com",
+    projectId: "coffee-app-430df",
+    storageBucket: "coffee-app-430df.firebasestorage.app",
+    messagingSenderId: "606685715487",
+    appId: "1:606685715487:web:d57359c4847d14e89c492a",
+  },
 
   // Firestore 集合名称，可保持默认。
   collection: 'coffee_sop_drinks',
